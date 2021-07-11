3710386a6b0ef7529bb4c32ec73ef1af788fa6b0
SenoVal <52401985+SenoVal@users.noreply.github.com>
2021-07-12 01:08:27 +0700

    Update divide.c

--- Calculator/Static/divide.c
+++ Calculator/Static/divide.c
@@ -1,1 +1,16 @@
+#include "mycalc.h"
 
+float divide(float Del) {
+	float a;
+	do {
+		printf("%.4f / ", Del);
+		scanf("%f", &a);
+		if( a == 0) {
+			printf("Wrong input \n");
+		}
+		else {
+			Del /= a;
+			return Del;
+		}
+	} while( a == 0);
+}
