b063eafed192354c41da04aa9a8469b79c3ec6a6
SenoVal <52401985+SenoVal@users.noreply.github.com>
2021-08-04 15:00:31 +0700

    Update sender.c

--- Homework/15. Socket/Multicast/sender.c
+++ Homework/15. Socket/Multicast/sender.c
@@ -17,26 +17,26 @@
 
 int main(){
 
-int fd;
-char buffer[80] = "Multicast msg";
-int len;
+	int fd;
+	char buffer[80] = "Multicast msg";
+	int len;
 
-struct sockaddr_in server;
-    
-if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
-	perror("Socket:");
-	exit(1);
-}
-memset(&server, 0, sizeof(server));
-server.sin_family = AF_INET;  
-server.sin_addr.s_addr = inet_addr(M_ADDR);
-server.sin_port = htons(M_PORT); 
-len = sizeof(server);
-	
-if(sendto(fd, buffer, strlen(buffer), 0, (struct sockaddr*)&server, len) == -1){
-    perror("Sendto:");
-    exit(1);
-}
-    
-return 0;
+	struct sockaddr_in server;
+
+	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+		perror("Socket:");
+		exit(1);
+	}
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;  
+	server.sin_addr.s_addr = inet_addr(M_ADDR);
+	server.sin_port = htons(M_PORT); 
+	len = sizeof(server);
+
+	if(sendto(fd, buffer, strlen(buffer), 0, (struct sockaddr*)&server, len) == -1){
+		perror("Sendto:");
+		exit(1);
+	}
+
+	return 0;
 }
