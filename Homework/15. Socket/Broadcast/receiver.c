1f5deff8d6897a344f36ab50f7ea38639156e70e
SenoVal <52401985+SenoVal@users.noreply.github.com>
2021-08-04 14:58:59 +0700

    Update receiver.c

--- Homework/15. Socket/Broadcast/receiver.c
+++ Homework/15. Socket/Broadcast/receiver.c
@@ -17,31 +17,31 @@
 
 int main(int argc, char *argv[]){
 
-int fd;
-char buffer[80];
-int len;
+	int fd;
+	char buffer[80];
+	int len;
     
-struct sockaddr_in server, client;
+	struct sockaddr_in server, client;
     
-if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-    perror("Socket:");
-    exit(1);
-} 
-memset(&server, 0, sizeof(server));
-server.sin_family = AF_INET;  
-server.sin_addr.s_addr = inet_addr(B_ADDRES);
-server.sin_port = htons(B_PORT);
-len = sizeof(server);
+	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
+    	perror("Socket:");
+    	exit(1);
+	} 
+	memset(&server, 0, sizeof(server));
+	server.sin_family = AF_INET;  
+	server.sin_addr.s_addr = inet_addr(B_ADDRES);
+	server.sin_port = htons(B_PORT);
+	len = sizeof(server);
 
-if(bind(fd, (struct sockaddr*)&server, sizeof(server)) == -1) {
-    perror("pthread_server Bind:");
-    exit(1);
-}
-	
-if(recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client, &len) == -1) {
-    perror("Recvfrom:");
-    exit(1);
-}
-    
-printf("%s\n", buffer);
+	if(bind(fd, (struct sockaddr*)&server, sizeof(server)) == -1){
+		perror("pthread_server Bind:");
+		exit(1);
+	}
+
+	if(recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client, &len) == -1){
+		perror("Recvfrom:");
+		exit(1);
+	}
+
+	printf("%s\n", buffer);
 }
