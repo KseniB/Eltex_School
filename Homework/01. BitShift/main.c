db1e52ca71790223941304e26039a5396e29ddfa
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2021-12-16 00:50:25 +0700

    Update main.c

--- Homework/01. BitShift/main.c
+++ Homework/01. BitShift/main.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <stdlib.h>
 
-uint32_t BitShift(uint32_t Number, uint8_t BS){
-	Number|=(0xFF<<(BS*8));
-	return Number;
+int bit_shift(int num)
+{
+    int shift = 0;
+	for (int i = 0; i < 4; i++)
+    {
+        shift = ((num >> i * 8) & 0xFF);
+        printf("Byte #%d: 0x%x\n", i, shift);
+    }
+	return 0;
 }
 
-int main(void){
-	uint32_t Num;
-	uint32_t Shift;
+int bit_replace(int num, int shift, int val)
+{
+    int b_num = 0x00000000;
+    
+    b_num = (b_num >> shift * 8) ^ 0xFF;
+    b_num = (b_num << shift * 8) ^ 0xFFFFFFFF;
+    
+	num = (num & b_num) ^ (val << shift * 8);
+	
+	printf("Result: %x \n", num);
+	puts("\n");
+	
+    return 0;
+}
 
-	printf("Your number in bytes (0xXXXXXXXX):");
-	scanf("%x", &Num);
-	printf("How many bytes to shift (0-3):");
-	scanf("%d", &Shift);
-
-	printf("Result: %x \n", BitShift(Num, (uint8_t)Shift));
+int main(void)
+{
+	unsigned int num = 0;
+	unsigned int shift = 0;
+	unsigned int val = 0;
+	
+    while(1){
+    	printf("Your number in bytes (0xXXXXXXXX):");
+    	scanf("%x", &num);
+    	bit_shift(num);
+    	
+    	printf("Which byte to replace? (0-3):");
+    	scanf("%x", &shift);
+    	
+    	printf("Enter a new byte (0xXX):");
+    	scanf("%x", &val);
+    	bit_replace(num, shift, val);
+    }
 	return 0;
 }
