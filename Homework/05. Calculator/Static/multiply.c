71b85a215d807c6220f7b797349edd4dbcecc9ec
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2021-12-26 04:47:03 +0700

    Update multiply.c

--- Homework/05. Calculator/Static/multiply.c
+++ Homework/05. Calculator/Static/multiply.c
@@ -1,9 +1,14 @@
 #include "mycalc.h"
 
-float multiply(float Pro){
-	float a;
-	printf("%.4f * ", Pro);
-	scanf("%f", &a);
-	Pro *= a;
-	return Pro;
+float multiply(float pro)
+{
+	float buf = 0;
+	system("clear");
+	printf("  +  -  *  /\n");
+	printf("       ---\n");
+	printf("%.4f * ", pro);
+	scanf("%f", &buf);
+	pro *= buf;
+	
+	return pro;
 }
