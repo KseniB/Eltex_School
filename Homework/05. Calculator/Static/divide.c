3352282141628f1e3d2f75ca5fa25fbb2fe84a2e
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2021-12-26 05:17:44 +0700

    Update divide.c

--- Homework/05. Calculator/Static/divide.c
+++ Homework/05. Calculator/Static/divide.c
@@ -1,15 +1,36 @@
 #include "mycalc.h"
 
-float divide(float Del){
-	float a;
-	do{
-		printf("%.4f / ", Del);
-		scanf("%f", &a);
-		if( a == 0) {
-			printf("Wrong input \n");
-		}else{
-			Del /= a;
-			return Del;
-		}
-	}while(a == 0);
+float divide(float del)
+{
+	float buf = 0;
+	system("clear");
+	printf("  +  -  *  /\n");
+	printf("          ---\n");
+	printf("%.4f / ", del);
+	if (1 != scanf("%f", &buf))
+	{
+		trashclean();
+		system("clear");
+		fprintf(stderr, "Invalid input");
+		puts("\nPress Enter to return to the menu");
+	    getchar();
+
+		return del;
+	}
+	
+	if(buf == 0)
+	{
+	    trashclean();
+		fprintf(stderr, "Invalid input");
+        puts("\nPress Enter to return to the menu");
+	    getchar();
+	    
+    	return del;
+	}
+	else
+	{
+		del /= buf;
+		
+		return del;
+	}
 }
