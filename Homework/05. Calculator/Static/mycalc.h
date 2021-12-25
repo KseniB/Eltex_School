6ab1fbdbc452d695455c95f91a80f71ce9ac2fe8
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2021-12-26 05:22:27 +0700

    Update mycalc.h

--- Homework/05. Calculator/Static/mycalc.h
+++ Homework/05. Calculator/Static/mycalc.h
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float plus(float Sum);
-float minus(float Raz);
-float multiply(float Pro);
-float divide(float Del);
+float plus(float sum);
+float minus(float razn);
+float multiply(float pro);
+float divide(float del);
+void menu();
+void trashclean();
