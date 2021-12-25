a0d026e5d742be4991de340bf44e5a329ea38b81
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2021-12-26 05:18:53 +0700

    Create menu.c

---
+++ Homework/05. Calculator/Static/menu.c
@@ -0,0 +1,9 @@
+void menu(float result)
+{
+    printf("1. Reset \n");
+    printf("2. Exit \n");
+    printf("--------------\n");
+    printf("Result = %.4f\n", result);
+    printf("--------------\n");
+    printf("Select: +  -  *  /\n");
+}
