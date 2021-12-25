2e4f2315a0709117a2a17797c5291d766921c06a
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2021-12-26 04:46:32 +0700

    Update minus.c

--- Homework/05. Calculator/Static/minus.c
+++ Homework/05. Calculator/Static/minus.c
@@ -1,9 +1,14 @@
 #include "mycalc.h"
 
-float minus(float Raz){
-	float a;
-	printf("%.4f - ", Raz);
-	scanf("%f", &a);
-	Raz -= a;
-	return Raz;
+float minus(float razn)
+{
+	float buf = 0;
+	system("clear");
+	printf("  +  -  *  /\n");
+	printf("    ---\n");
+	printf("%.4f - ", razn);
+	scanf("%f", &buf);
+	razn -= buf;
+	
+	return razn;
 }
