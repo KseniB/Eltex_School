c3a8b7b3825aa3e7a8bcb02e1a346a693f122295
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2021-12-26 04:45:44 +0700

    Update plus.c

--- Homework/05. Calculator/Static/plus.c
+++ Homework/05. Calculator/Static/plus.c
@@ -1,9 +1,14 @@
 #include "mycalc.h"
 
-float plus(float Sum){
-	float a;
-	printf("%.4f + ", Sum);
-	scanf("%f", &a);
-	Sum += a;
-	return Sum;
+float plus(float sum)
+{
+	float buf = 0;
+	system("clear");
+	printf("  +  -  *  /\n");
+	printf(" ---\n");
+	printf("%.4f + ", sum);
+	scanf("%f", &buf);
+	sum += buf;
+	
+	return sum;
 }
