8bf0b618a3b4ab8cd17675ee2aa489184a7bbed8
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2021-12-27 13:24:20 +0700

    Update and rename Homework/05. Calculator/Dinamic/mycalc.h to Homework/05. Calculator/Dinamic/lib/mycalc.h

---
+++ Homework/05. Calculator/Dinamic/lib/mycalc.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float plus(float sum);
+float minus(float razn);
+float multiply(float pro);
+float divide(float del);
+void menu(float result);
+void trashclean();
--- Homework/05. Calculator/Dinamic/mycalc.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-float plus(float Sum);
-float minus(float Raz);
-float multiply(float Pro);
-float divide(float Del);
