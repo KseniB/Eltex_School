5069289d1291341c3c1eb9c9e8890bd31c374aa3
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2022-01-07 16:31:14 +0700

    Update main.c

--- Homework/08. Pid/main.c
+++ Homework/08. Pid/main.c
@@ -3,37 +3,51 @@
 #include <sys/wait.h>
 
 /*
-Ствол (Родит.проц.)                [p1 trunk]
-                                  /        \
-Ветви (Доч.проц.)           [p2 branch]  [p3 branch]
-                            /       \           \
-Листья (Доч.проц.)    [p4 leaves] [p5 leaves]  [p6 leaves]
+Ствол (Родит.проц.)                    [p1 trunk]
+                                      /        \
+Ветви (Доч.проц.)               [p2 branch]  [p3 branch]
+                                 /       \           \
+Листья (Потом.доч.проц.)    [p4 leaves] [p5 leaves]  [p6 leaves]
 */
 
-int main(){
-  
+int main()
+{
     pid_t branchPid[2];
     pid_t leavesPid[2];
-    int branch_i;
-    int leaves_i;
+    /*Переменная для получения двух потомком в первой ветви и одного во второй:*/
     int population = 2;
-  
-    for (branch_i = 0; branch_i < 2; branch_i++){
-        if ((branchPid[branch_i] = fork()) == 0){
-            for (leaves_i = 0; leaves_i < population; leaves_i++){
-                if ((leavesPid[leaves_i] = fork()) == 0){
-                    printf("Leaves  PPID:%d PID:%d\n", getppid(), getpid());
+    int index = 1;
+    
+    printf(" p%d Trunk   PID:%d\n", index, getpid());
+
+    for (int branch_i = 0; branch_i < 2; branch_i++)
+    {
+        index++;
+        if ((branchPid[branch_i] = fork()) == 0)
+        {
+            printf("\tp%d Branch  PPID:%d PID:%d\n", index, getppid(), getpid());
+            index++;
+            if (branch_i == 1){
+                index++;
+            }
+            
+            for (int leaves_i = 0; leaves_i < population; leaves_i++)
+            {
+                index++;
+                if ((leavesPid[leaves_i] = fork()) == 0)
+                {
+                    printf("\t\tp%d Leaves  PPID:%d PID:%d\n", index, getppid(), getpid());
+                    
                     return 0;
                 }
-            waitpid(leavesPid[leaves_i], NULL, WUNTRACED);
+                waitpid(leavesPid[leaves_i], NULL, WUNTRACED);
             }
-        printf("Branch  PPID:%d PID:%d\n", getppid(), getpid());
-        return 0;
+            
+            return 0;
         }
-    waitpid(branchPid[branch_i], NULL, WUNTRACED);
-    population--;
+        waitpid(branchPid[branch_i], NULL, WUNTRACED);
+        population--;
     }
-  
-    printf("Trunk   PID:%d\n", getpid());
+    
     return 0;
 }
