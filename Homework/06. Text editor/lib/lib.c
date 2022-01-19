f3f8021e430a846cd05a356778f9b064e2b78081
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2022-01-19 12:29:04 +0700

    Update lib.c

--- Homework/06. Text editor/lib/lib.c
+++ Homework/06. Text editor/lib/lib.c
@@ -1,169 +1,54 @@
 #include "lib.h"
 
-#define MAX_SIZE 50
-
-extern WINDOW* btmwnd;
-extern int row, col, line;
-extern int fd, width, height;
-extern char filename[MAX_SIZE];
-
-void sig_winch(int signo)
+char open_file(char *file_name, struct winsize size)
 {
-	struct winsize size;
-	ioctl(fileno(stdout), TIOCGWINSZ, (char *) &size);
-	resizeterm(size.ws_row, size.ws_col);
-}
-
-void start()
-{
-	start_color();
-	refresh();
-	echo();
+	int file;
+	file = open(file_name, O_CREAT, O_RDWR);
+	char buff[81];
 	
-	init_pair(1, COLOR_WHITE, COLOR_BLACK);
-	btmwnd = newwin(1, width, height - 3, 0);
-	wbkgd(btmwnd, COLOR_PAIR(1));
-	wprintw(btmwnd, " <F1> - Open file  <F2> - Save changes  <F3> - Exit\t");
-	wrefresh(btmwnd);
-}
-
-int len(int lineno)
-{
-	int linelen = COLS - 1;
-	while (linelen >= 0 && mvinch(lineno, linelen) == ' ')
+	if (file == -1)
 	{
-	    linelen--;
+		printw("File wrong");
 	}
-	return linelen + 1;
-}
-
-void openfile()
-{
-    WINDOW* win;
-	char c;
-	refresh();
-	echo();
-	start_color();
-	
-	init_pair(1, COLOR_BLACK, COLOR_WHITE);
- 	win = newwin(1, MAX_SIZE, height - 5, 0);
-	wattron(win, COLOR_PAIR(1));
-    wprintw(win, "Enter file name: ");
-	wgetnstr(win, filename, MAX_SIZE);
-    	
-	wrefresh(win);
-	delwin(win);
-	clear();
-	start();
-	
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
+	else
 	{
-	    endwin();
-	    perror("openfile()");
-		exit(EXIT_FAILURE);
-	}
-        
-	while (1)
-	{
-		if (read(fd, &c, 1) == 1)
+		move(0, 0);
+		for (int i = 0; i < size.ws_row - 2; i++)
 		{
-		    addch(c);
-		    if (c == '\n')
-		    {
-		        line++;
-		    }
+			memset(buff, 0 , 81);
+			read(file, buff, 80);
+			move(i, 0);
+			printw("%s", buff);
 		}
-		else break;
-	}  
-	close(fd);
+		move(size.ws_row - 1, 0);
+		printw("File '%s' is opened! Press 'esc' to exit", file_name);
+	}
+	close(file);
 }
 
-void savech()
+void draw_input(char *file_name, struct winsize size)
 {
-	int length;
-    char c;
-    
-	fd = open(filename, O_WRONLY | O_TRUNC);
-	if (fd < 0)
-	{
-	    endwin();
-	    perror("savech()");
-		exit(EXIT_FAILURE);
-	}
-    for (int i = 0; i < LINES - 4; i++)
-    {
-	    length = len(i);
-	    for (int j = 0; j < length; j++)
-	    {
-	        c = mvinch(i, j) & A_CHARTEXT;
-	        write(fd, &c, 1);
-	    }
-	    write(fd, "\n", 1);
-	}
-    close(fd);
+    move(size.ws_row - 1, 0);
+    printw("Input file name: ");	
+    refresh();
+    nocbreak();
+    echo();
+    wgetnstr(stdscr, file_name, 30);
+    move(size.ws_row - 1, 0);
+    printw("                                                  \0");
+    move(size.ws_row - 1, 0);
 }
 
-void keypad_func()
+int start_draw(struct winsize size)
 {
-    noecho();
-    int ch = getch();
-
-	switch(ch){	
-		
-		case KEY_UP:
-		    if (row > 0)
-		    {
-		        row--;
-		    }
-        break;
-
-		case KEY_DOWN:
-		    row++;
-	    break;
-
-		case KEY_LEFT:
-		    if (col > 0)
-		    {
-		        col--;
-		    }
-	    break;
-
-		case KEY_RIGHT:
-		    col++;
-	    break;
-
-		case KEY_F(1):
-		    openfile(O_RDONLY);
-	    break;
-
-		case KEY_F(2):
-		    savech();
-	    break;
-
-		case KEY_F(3):
-		    close(fd);
-		    delwin(btmwnd);
-		    endwin();
-		    exit(EXIT_SUCCESS);
-	    break;
-	    
-		case KEY_BACKSPACE:
-		    if (col >= 0)
-		    {
-		        delch();
-		        col--;
-		    }
-		    if (col < 0)
-		    {
-		        col++;
-		    }
-	    break;
-
-		default:
-		    insch(ch);
-		    move(row, col++);
-		    refresh();
-	    break;
-	}
+    int t;
+    cbreak;
+    curs_set(FALSE);
+    move(size.ws_row - 1, 0);
+    printw("Open file - F1 | Save file - F2 | Exit - F3");
+    move(size.ws_row - 1, 0);
+    t = getch();
+    printw("                                                                                                                   \0");	
+    refresh();	
+    return t;
 }
