dae486d3c32f8a97ec21b3dc239d96316bb14ee6
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2022-01-19 12:28:24 +0700

    Update lib.h

--- Homework/06. Text editor/lib/lib.h
+++ Homework/06. Text editor/lib/lib.h
@@ -1,17 +1,20 @@
 #pragma once
 
+#include <stdio.h>
 #include <string.h>
-#include <sys/ioctl.h>
-#include <signal.h>
-#include <stdlib.h>
-#include <curses.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <termios.h>
 
-void sig_winch(int signo);
-void start();
-int len(int lineno);
-void openfile();
-void savech();
-void keypad_func();
+#include <termios.h> 
+#include <sys/ioctl.h> 
+#include <signal.h> 
+#include <stdlib.h> 
+#include <curses.h> 
+
+struct winsize size;
+
+char open_file(char *file_name, struct winsize size);
+void draw_input(char *file_name, struct winsize size);
+int start_draw(struct winsize size);
