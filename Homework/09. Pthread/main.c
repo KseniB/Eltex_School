85d8bdadfff98bc95a3afd0884a0dccbb29c8d30
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2022-01-25 19:17:18 +0700

    Update main.c

--- Homework/09. Pthread/main.c
+++ Homework/09. Pthread/main.c
@@ -5,31 +5,38 @@
 
 int a = 0;
 
-void *func(void *args){
+void *func(void *args)
+{
     int tmp = 0;
 
-    for(int i = 0; i < 1000; i++){
+    for(int i = 0; i < 1000; i++)
+    {
         tmp = a;
         tmp++;
         a = tmp;
     }
 }
 
-int main(void){
-    
+int main(void)
+{
     pthread_t tid[1000];
     int ret = 0;
 
-    for(int i = 1; i <= 1000; i++){
+    for(int i = 1; i <= 1000; i++)
+    {
         ret = pthread_create(&tid[i - 1], NULL, func, NULL);
-        if(ret != 0){
+        if(ret != 0)
+        {
             perror("create");
             exit(EXIT_FAILURE);
         }
     }
-    for(int i = 1; i <= 1000; i++){
+    
+    for(int i = 1; i <= 1000; i++)
+    {
         ret = pthread_join(tid[i - 1], NULL);
-        if(ret != 0){
+        if(ret != 0)
+        {
             perror("join");
             exit(EXIT_FAILURE);
         }
