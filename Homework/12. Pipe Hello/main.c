ebf084e0e46724f1633303ce682337f31adf06d1
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2022-01-28 05:12:28 +0700

    Update main.c

--- Homework/12. Pipe Hello/main.c
+++ Homework/12. Pipe Hello/main.c
@@ -1,69 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
 #include <fcntl.h>
-#include <errno.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <pthread.h>
 
-unsigned char* Str_get(unsigned int* l){
-    
-	*l = 0;
+unsigned char* str_get(unsigned int* len)
+{
+	*len = 0;
 	unsigned int cap = 1;
-	unsigned char* s = (unsigned char*) malloc(10 * sizeof(unsigned char));
-	unsigned char  c = getchar();
+	unsigned char* str = (unsigned char*) malloc(10 * sizeof(unsigned char));
+	unsigned char  ch = getchar();
 
-	while(c != '\n'){
-		s[(*l)++] = c;
-		if(*l >= 10){
+	while (ch != '\n')
+	{
+		str[(*len)++] = ch;
+		if (*len >= 10)
+		{
 			cap *= 2;
-			s = (unsigned char*) realloc(s, cap * sizeof(unsigned char));
+			str = (unsigned char*) realloc(str, cap * sizeof(unsigned char));
 		}
-		c = getchar();
+		ch = getchar();
 	}
-	s[*l] = '\0';
-	return s;
+	
+	str[*len] = '\0';
+	return str;
 }
 
-int main(void){
-    
-    pid_t _pid;
+int main(void)
+{
+    pid_t pid;
+    int fd[2];
     int rv;
-    int fid[2];
-    _pid = fork();
     
-    if(_pid == 0){
+    pid = fork();   
+    if (pid == 0)
+    {
         int f_w = open("writefile.txt", O_WRONLY|O_APPEND, 0644);
-        if(f_w < 0){
+        if (f_w < 0)
+        {
             perror("OPEN writefile.txt");
             exit(EXIT_FAILURE);
         }
-        printf("Enter word\n");
-        unsigned int g = 50;
-        if(pipe(fid) < 0){
+        
+        printf("Enter word:\n");
+        unsigned int str_len = 50;
+        
+        if (pipe(fd) < 0)
+        {
             perror("Pipe error");
         }
+        
         dup2(f_w, 1);
-    
-        unsigned char* str = Str_get(&g);
+        unsigned char* str = str_get(&str_len);
         printf("%s\n", str);
         exit(rv);
-    }else{
+    }
+    else
+    {
         wait(&rv);
         int f_r = open("writefile.txt", O_RDONLY);
-        if(f_r < 0){
+        
+        if (f_r < 0)
+        {
             perror("READ writefile.txt");
             exit(EXIT_FAILURE);
         }
+        
         unsigned char buf[1];
         size_t byte_r;
         printf("READ FILE:\n");
-        while((byte_r = read(f_r, buf, 1)) > 0){
+        
+        while ((byte_r = read(f_r, buf, 1)) > 0)
+        {
             printf("%c", buf[0]);
         }
     }
-  
+    
+    close(fd[0]);
+    close(fd[1]);
     return 0;
 }
