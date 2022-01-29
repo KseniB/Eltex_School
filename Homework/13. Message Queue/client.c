b59468c5fb6cc022e91c1efdc52c907d4f85deac
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2022-01-29 17:36:52 +0700

    Update client.c

--- Homework/13. Message Queue/client.c
+++ Homework/13. Message Queue/client.c
@@ -11,23 +11,28 @@
 #define SERVER_SERVICE_QUEUE "/server_service"
 #define MSG_BUF_SIZE 100
 
-void *check_self_queue(void *param){
+void *check_self_queue(void *param)
+{
     mqd_t client_descriptor = *((mqd_t *)param);
     char inbox[MSG_BUF_SIZE];
 
-    while(1){
-        if(mq_receive(client_descriptor, inbox, MSG_BUF_SIZE, NULL) > 0){
-        printf("%s", inbox);
-        memset(inbox, 0, sizeof(inbox));
+    while (1)
+    {
+        if (mq_receive(client_descriptor, inbox, MSG_BUF_SIZE, NULL) > 0)
+        {
+            printf("%s", inbox);
+            memset(inbox, 0, sizeof(inbox));
         }
     }
 }
 
 
-int main(){
+int main()
+{
     mqd_t chat_descriptor, service_descriptor, client_descriptor;
     pid_t pid;
     pthread_t check_self_queue_tid;
+    
     char input[MSG_BUF_SIZE - 10];
     char message[MSG_BUF_SIZE + 2];
     char nickname[10];
@@ -40,14 +45,14 @@ int main(){
 
     printf("[CLIENT] - Started\n");
 
-    while((chat_descriptor = mq_open(SERVER_CHAT_QUEUE, O_WRONLY)) == -1){}
+    while ((chat_descriptor = mq_open(SERVER_CHAT_QUEUE, O_WRONLY)) == -1){}
     printf("[CLIENT] - Chat queue opened\n");
-    while((service_descriptor = mq_open(SERVER_SERVICE_QUEUE, O_WRONLY)) == -1){}
+    while ((service_descriptor = mq_open(SERVER_SERVICE_QUEUE, O_WRONLY)) == -1){}
 
     pid = getpid();
     sprintf(message, "%d", pid);
-    while(mq_send(service_descriptor, message, strlen(message), 0) != 0){}
-    while(mq_close(service_descriptor) == -1){}
+    while (mq_send(service_descriptor, message, strlen(message), 0) != 0){}
+    while (mq_close(service_descriptor) == -1){}
 
     printf("Enter your nickname: ");
     fgets(nickname, 10, stdin);
@@ -55,21 +60,23 @@ int main(){
     *char_position = '\0';
 
     sprintf(self_descriptor, "/client_%d", pid);
-    while((client_descriptor = mq_open(self_descriptor, O_RDONLY)) == -1){}
+    while ((client_descriptor = mq_open(self_descriptor, O_RDONLY)) == -1){}
 
-    while(pthread_create(&check_self_queue_tid, &threadAttr, check_self_queue, (void *)&client_descriptor) != 0){}
+    while (pthread_create(&check_self_queue_tid, &threadAttr, check_self_queue, (void *)&client_descriptor) != 0){}
 
     printf("--------CHAT--------\n");
-    while(1){
+    while (1)
+    {
         fgets(input, (MSG_BUF_SIZE - 10), stdin);
-        if(strcmp(input, "/shut\n") == 0){
-        break;
+        if (strcmp(input, "/shut\n") == 0)
+        {
+            break;
         }
     }
 
-    while(mq_close(chat_descriptor) == -1){}
+    while (mq_close(chat_descriptor) == -1){}
     printf("[CLIENT] - Chat queue closed\n");
-    while(mq_close(client_descriptor) == -1){}
+    while (mq_close(client_descriptor) == -1){}
 
     printf("[CLIENT] - Shuted down\n");
     exit(0);
