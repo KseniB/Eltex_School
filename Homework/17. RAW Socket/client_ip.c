995f71855dd4f205c3db6d917c66e60970753fd6
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2022-01-31 04:46:22 +0700

    Update client_ip.c

--- Homework/17. RAW Socket/client_ip.c
+++ Homework/17. RAW Socket/client_ip.c
@@ -11,14 +11,17 @@
 #define PORT 8080
 #define S_PORT 8081
 #define SIZE_PACKAGE 128
+#define ADDR 127.0.0.1
 
-struct package{
+struct package
+{
 	char network[20];
 	char udphdr[8];
 	char msg[100];
 };
 
-struct iphdr{
+struct iphdr
+{
 	char vers_sizeh;
 	char hz;
 	short size_packege;
@@ -31,21 +34,23 @@ struct iphdr{
 	int d_ip;
 };
 
-struct udphdr{
-    u_short	uh_sport;		// Source port 
-	u_short	uh_dport;		// Destination port
-	u_short	uh_ulen;		// Length
-	u_short	uh_sum;			// Checksum
+struct udphdr
+{
+    u_short	uh_s_port;		// Source port 
+	u_short	uh_d_port;		// Destination port
+	u_short	uh_len;		    // Length
+	u_short	uh_chsum;		// Checksum
 };
 
-
-int main(int argc, char *argv[]){
-    
+int main()
+{
 	int fd, fd1;
 	char buffer[128];
 	int len;
 	short *dport;
+	
 	memset(buffer, 0, 128);
+	
 	struct sockaddr_in server, client;
 	struct udphdr *header;
 	struct package *receive;
@@ -56,36 +61,45 @@ int main(int argc, char *argv[]){
 	ip_hdr->offset = 0x0040;
 	ip_hdr->ttl = 0x40;
 	ip_hdr->proto = 0x11;
-	ip_hdr->s_ip = inet_addr("127.0.0.1");
-	ip_hdr->d_ip = inet_addr("127.0.0.1");
+	ip_hdr->s_ip = inet_addr(ADDR);
+	ip_hdr->d_ip = inet_addr(ADDR);
 
 	header = (struct udphdr*)&buffer[20];
-	header->uh_sport = htons(S_PORT);
-	header->uh_dport = htons(PORT);
-	header->uh_ulen  = htons(SIZE_PACKAGE - 20);
-	header->uh_sum   = htons(0);
+	header->uh_s_port = htons(S_PORT);
+	header->uh_d_port = htons(PORT);
+	header->uh_len    = htons(SIZE_PACKAGE - 20);
+	header->uh_chsum  = htons(0);
 
 	strcat(&buffer[28], argv[1]);
-	client.sin_family    = AF_INET;  
+	
+	client.sin_family = AF_INET;  
 	client.sin_addr.s_addr = INADDR_ANY;
 	client.sin_port = htons(S_PORT);
 
-	if((fd1 = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
-		perror("Socket:");
-		exit(1);
+	if ((fd1 = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
 	}
-	if(bind(fd1, (struct sockaddr*)&client, sizeof(client)) == -1){
-		perror("Bind");
-		exit(1);
+	
+	if (bind(fd1, (struct sockaddr*)&client, sizeof(client)) == -1
+	{
+		perror("bind");
+		exit(EXIT_FAILURE);
 	}
-    if((fd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) == -1){
-        perror("Socket:");
-        exit(1);
+	
+    if ((fd = socket(AF_INET, SOCK_RAW, IPPROTO_UDP)) == -1)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
     }
-    if(setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &(int){1}, sizeof(int)) == -1){
-	    perror("Setsockopt");
-	    exit(1);
+    
+    if (setsockopt(fd, IPPROTO_IP, IP_HDRINCL, &(int){1}, sizeof(int)) == -1)
+    {
+	    perror("setsockopt");
+	    exit(EXIT_FAILURE);
   	}
+  	
 	memset(&server, 0, sizeof(server));
 
     server.sin_family = AF_INET;  
@@ -93,21 +107,29 @@ int main(int argc, char *argv[]){
     server.sin_port = htons(PORT);
 
 	len = sizeof(server);
-	if(sendto(fd, buffer, SIZE_PACKAGE, 0, (struct sockaddr*)&server, len) == -1){
-        perror("Sendto:");
-        exit(1);
+	
+	if (sendto(fd, buffer, SIZE_PACKAGE, 0, (struct sockaddr*)&server, len) == -1)
+	{
+        perror("sendto");
+        exit(EXIT_FAILURE);
  	}
+ 	
  	receive = (struct package*)buffer;
 	dport = (short *)&receive->udphdr[2];
- 	while(1){
-		if(recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&server, &len) == -1){
-		    perror("Recvfrom:");
-		    exit(1);
+	
+ 	while (1)
+ 	{
+		if (recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&server, &len) == -1)
+		{
+		    perror("recvfrom");
+		    exit(EXIT_FAILURE);
 		}
-		if(ntohs(*dport) == S_PORT) {
+		if (ntohs(*dport) == S_PORT) {
 			printf("It's mine: ");
 			printf("%s\n", receive->msg);
-		}else{
+		}
+		else
+		{
 			printf("That's not mine: ");
 			printf("%s\n", receive->msg);
 		}
