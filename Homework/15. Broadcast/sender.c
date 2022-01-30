96359eef887ea9fe93da3a043a0d10d1e86eee15
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2022-01-30 16:38:40 +0700

    Update sender.c

--- Homework/15. Broadcast/sender.c
+++ Homework/15. Broadcast/sender.c
@@ -7,7 +7,6 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
-#include <mqueue.h>
 #include <fcntl.h>          
 #include <sys/stat.h>
 
@@ -17,29 +16,35 @@
 
 int main(){
 
-	char buffer[80] = "broadcast msg";
+	char buffer[80] = "Broadcast msg";
 	int fd;
 	int len;
 
 	struct sockaddr_in server;
 
-	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
-		perror("Socket:");
-		exit(1);
+	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+	{
+		perror("socket");
+		exit(EXIT_FAILURE);
 	}
-	if(setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &(int){1}, sizeof(int)) == -1){
+	if (setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &(int){1}, sizeof(int)) == -1)
+	{
 		perror("setsockopt");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
+	
 	memset(&server, 0, sizeof(server));
-	server.sin_family    = AF_INET;  
+	
+	server.sin_family = AF_INET;  
 	server.sin_addr.s_addr = inet_addr(B_ADDRES);
 	server.sin_port = htons(B_PORT); 
+	
 	len = sizeof(server);
 
-	if(sendto(fd, buffer, strlen(buffer), 0, (struct sockaddr*)&server, len) == -1){
-		perror("Sendto:");
-		exit(1);
+	if (sendto(fd, buffer, strlen(buffer), 0, (struct sockaddr*)&server, len) == -1)
+	{
+		perror("sendto");
+		exit(EXIT_FAILURE);
 	}
 
 	return 0;
