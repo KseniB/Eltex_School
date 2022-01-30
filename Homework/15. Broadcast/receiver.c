e6f3a08b29b39b27d69cd31df6f307c52bdfda02
Boltunova Kseniya <52401985+KseniB@users.noreply.github.com>
2022-01-30 16:39:16 +0700

    Update receiver.c

--- Homework/15. Broadcast/receiver.c
+++ Homework/15. Broadcast/receiver.c
@@ -7,7 +7,6 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
-#include <mqueue.h>
 #include <fcntl.h>          
 #include <sys/stat.h>
 
@@ -15,32 +14,38 @@
 #define B_PORT 7777
 #define B_ADDRES "224.0.0.1"
 
-int main(int argc, char *argv[]){
-
+int main()
+{
 	int fd;
 	char buffer[80];
 	int len;
     
 	struct sockaddr_in server, client;
     
-	if((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1){
-    	perror("Socket:");
-    	exit(1);
+	if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) == -1)
+	{
+    	perror("socket");
+    	exit(EXIT_FAILURE);
 	} 
+	
 	memset(&server, 0, sizeof(server));
+	
 	server.sin_family = AF_INET;  
 	server.sin_addr.s_addr = inet_addr(B_ADDRES);
 	server.sin_port = htons(B_PORT);
+	
 	len = sizeof(server);
 
-	if(bind(fd, (struct sockaddr*)&server, sizeof(server)) == -1){
-		perror("pthread_server Bind:");
-		exit(1);
+	if (bind(fd, (struct sockaddr*)&server, sizeof(server)) == -1)
+	{
+		perror("bind");
+		exit(EXIT_FAILURE);
 	}
 
-	if(recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client, &len) == -1){
-		perror("Recvfrom:");
-		exit(1);
+	if (recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client, &len) == -1)
+	{
+		perror("recvfrom");
+		exit(EXIT_FAILURE);
 	}
 
 	printf("%s\n", buffer);
